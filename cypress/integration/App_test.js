71422eaa51c6dd4cc847a6170f402464b59f74ed
Anil_Ramcharran <anil.gitme@gmail.com>
2020-12-14 21:45:09 -0500

    fixed one of the test code

--- cypress/integration/App_test.js
+++ cypress/integration/App_test.js
@@ -4,7 +4,7 @@ describe('Testing the application', () => {
     })
     it('Mvp test', () => {
         cy.get('input[name="name"]').type("Anil").should('have.value', "Anil")
-        cy.get("#size").select('Large').should('have.value', "Large")
+        cy.get("#size").select('Large').should('have.value', "Large pizza")
         cy.get('input[name="jalapenos"]').check().should('be.checked')
         cy.get('input[name="pepperoni"]').check().should('be.checked')
         cy.get('input[name="ham"]').check().should('be.checked')
